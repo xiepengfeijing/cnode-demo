44d525c6fce9c954a994fc7c866e3a445a289f96
Xu Jing <734332894@qq.com>
2018-06-25 16:56:01 +0800

    统一静态资源处理

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 var express = require('express')
 var router = require('./router')
-
-
+// var art = require('express-art-template')
 var app = express()
+var bodyParser = require('body-parser');
+app.engine('html',require('express-art-template'));
+app.use(bodyParser.urlencoded({extend:false}));
+app.use(bodyParser.json());
+
+
+app.use('/public',express.static('./public'))
+// app.use(express.static('./views'))
+app.use('/node_modules',express.static('./node_modules'))
 
 app.use(router)
 app.listen(3000, () => {
--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 exports.showSignin = (req, res) => {
-    res.send("showSignin")
+    res.render('signin.html')
 }
 
 exports.handleSignin = (req, res) => {
